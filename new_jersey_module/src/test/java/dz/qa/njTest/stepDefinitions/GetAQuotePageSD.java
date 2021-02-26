f1be2887c0e835a22d5d934407827ae474db9ae4
kmekdoud <karimmekdoud@gmail.com>
2021-02-25 22:47:30 -0500

    karim ++

--- new_jersey_module/src/test/java/dz/qa/njTest/stepDefinitions/GetAQuotePageSD.java
+++ new_jersey_module/src/test/java/dz/qa/njTest/stepDefinitions/GetAQuotePageSD.java
@@ -4,8 +4,7 @@ import dz.qa.njPages.GetAQuotePage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
-import static dz.qa.njTest.testbase.SetUp.getAQuotePage;
+import static dz.qa.njTest.testbase.SetUp.*;
 
 public class GetAQuotePageSD extends GetAQuotePage {
     @Given("the user Lands in Stat farm home page")
@@ -41,8 +40,12 @@ public class GetAQuotePageSD extends GetAQuotePage {
     public void user_click_on_next_Vehicles_button() throws InterruptedException {
         getAQuotePage.user_click_on_next_Vehicles_button_Step();
     }
+//    @Then("User should see the next form {string}")
+//    public void user_should_see_the_next_form(String string) throws InterruptedException {
+//        getAQuotePage.user_should_see_the_next_form_Step(string);
+//    }
     @Then("User should see the next form {string}")
     public void user_should_see_the_next_form(String string) throws InterruptedException {
-        getAQuotePage.user_should_see_the_next_form_Step(string);
+        getAutoQuote.user_should_see_the_next_form_Step(string);
     }
 }
--- new_jersey_module/src/test/java/dz/qa/njTest/testbase/SetUp.java
+++ new_jersey_module/src/test/java/dz/qa/njTest/testbase/SetUp.java
@@ -1,10 +1,6 @@
 package dz.qa.njTest.testbase;
 import common.WebAPI;
-import dz.qa.njPages.GetAQuotePage;
-import dz.qa.njPages.HomePage;
-import dz.qa.njPages.LoginPage;
-import dz.qa.njPages.Term_LifePage;
-import dz.qa.njPages.RentersQuotePage;
+import dz.qa.njPages.*;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -23,12 +19,15 @@ public class SetUp extends WebAPI {
     public static Term_LifePage term_LifePage;
     public static GetAQuotePage getAQuotePage;
     public static RentersQuotePage rentersQuotePage;
+    public static GetAutoQuote getAutoQuote;
+
     public static void Init() {
         term_LifePage = PageFactory.initElements(driver, Term_LifePage.class);
         homePage = PageFactory.initElements(driver, HomePage.class);
         loginPage = PageFactory.initElements(driver, LoginPage.class);
         getAQuotePage = PageFactory.initElements(driver,GetAQuotePage.class);
         rentersQuotePage = PageFactory.initElements(driver,RentersQuotePage.class);
+        getAutoQuote = PageFactory.initElements(driver,GetAutoQuote.class);
     }
     @Before
     public void setUp_Init() throws IOException {
